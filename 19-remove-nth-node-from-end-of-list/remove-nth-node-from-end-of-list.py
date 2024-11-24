f991aecc8e71b998ba1ac071bf6c0482934f348f
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2024-11-24 13:31:36 +0500

    Time: 0 ms (100.00%) | Memory: 16.5 MB (65.33%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -7,20 +7,38 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         # 1st approach two pass
 
-        count = 0 # to find length of the list
+        # count = 0 # to find length of the list
+        # temp = head
+        # while temp:
+        #     count += 1
+        #     temp = temp.next
+        # if count == n: # head to be deleted
+        #     return head.next
+        
+        # temp = head
+        # traverse_from_front = count - n # l-n
+        # while traverse_from_front != 1: # stop at node one before the node to be deleted
+        #     temp = temp.next
+        #     traverse_from_front -= 1
+        
+        # del_node = temp.next
+        # temp.next = del_node.next
+        # return head        
+
+        # 2nd approach one pass
         temp = head
-        while temp:
-            count += 1
+        for i in range(n): # move temp n steps
             temp = temp.next
-        if count == n: # head to be deleted
+
+        if temp == None:
             return head.next
-        
-        temp = head
-        traverse_from_front = count - n # l-n
-        while traverse_from_front != 1: # stop at node one before the node to be deleted
+
+        prev = head
+        while temp.next:
+            prev = prev.next
             temp = temp.next
-            traverse_from_front -= 1
         
-        del_node = temp.next
-        temp.next = del_node.next
+        # prev will be the node one before the node to be deleted
+        del_node = prev.next
+        prev.next = del_node.next
         return head        
