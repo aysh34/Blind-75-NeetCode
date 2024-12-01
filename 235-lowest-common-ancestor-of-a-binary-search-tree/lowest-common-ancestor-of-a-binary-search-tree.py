612408eca86296c928d828bcca60231ef435bb8a
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2024-12-01 20:56:20 +0500

    Time: 64 ms (9.91%) | Memory: 20.8 MB (5.34%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        # LCA: The "lowest" means the node that is deepest (farthest from the root) in the tree while still being a common ancestor. level 0 (L0) is considered higher than level 1 (L1) in a tree!
+        if not root:
+            return None
+
+        if root.val == p.val or root.val == q.val:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        
+        return left if left else right
