5730e66ac3d6ae56fecde951f61140518fcb00a7
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2024-12-05 18:40:41 +0500

    Update kth-smallest-element-in-a-bst.py

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,6 +6,25 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # 1st approach 
+        res = self.inOrder(root)
+        return res[k-1]
+        # c = 1
+        # for i in res:
+        #     if c == k:
+        #         return i
+        #     else:
+        #         c += 1
+    
+    def inOrder(self,root):
+        if not root:
+            return []
+        return (
+            self.inOrder(root.left) + [root.val] + self.inOrder(root.right)
+        ) # TC: O(n), SC: O(n) extra space
+
+        # optimized approach
+        
         self.count = 0 # instance variables so they retain their values across recursive calls.
         self.res = None
         self.inOrder(root,k)
@@ -22,6 +41,6 @@ class Solution:
             self.res = root.val 
             return # stop further traversing
 
-        self.inOrder(root.right,k)
+        self.inOrder(root.right,k) # SC: O(h) where h is the height of the tree. This is the space used by the recursion stack, which can be O(log n) for a balanced tree and O(n) for a skewed tree.
     
 
